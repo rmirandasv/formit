08d88458f9a7b06d95b08b0065268cd888bf9c38
Ronald Miranda <ronaldmirandasv@gmail.com>
2024-11-02 11:37:03 -0600

    tailwindcss setup

--- resources/ts/pages/home/index.tsx
+++ resources/ts/pages/home/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gray-200 flex content-center">
+    <div className="min-h-screen bg-gray-300 flex items-center justify-center">
       <h1 className="text-2xl font-semibold">Home Page</h1>
     </div>
   );
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    @vite('resources/ts/app.tsx')
+    @vite(['resources/css/app.css', 'resources/ts/app.tsx'])
     @inertiaHead
   </head>
   <body>
