ccb579adc836429552ec4448cb25ff2aeefbf717
Ronald Miranda <ronaldmirandasv@gmail.com>
2024-11-03 23:42:39 -0600

    sidebar

--- resources/ts/components/app/navigation/app-sidebar.tsx
+++ resources/ts/components/app/navigation/app-sidebar.tsx
@@ -1,20 +1,77 @@
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
 import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
   SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
   SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
 } from "@/components/ui/sidebar";
+import { Link } from "@inertiajs/react";
+
+const items = [
+  {
+    title: "Home",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "Inbox",
+    url: "#",
+    icon: Inbox,
+  },
+  {
+    title: "Calendar",
+    url: "#",
+    icon: Calendar,
+  },
+  {
+    title: "Search",
+    url: "#",
+    icon: Search,
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: Settings,
+  },
+];
 
 export function AppSidebar() {
   return (
-    <Sidebar>
-      <SidebarHeader />
-      <SidebarContent>
-        <SidebarGroup />
+    <Sidebar className="border-r-slate-700">
+      <SidebarHeader className="bg-slate-800">
+        <Link href="/" className="text-white text-2xl">
+          Formit
+        </Link>
+      </SidebarHeader>
+      <SidebarContent className="bg-slate-800">
+        <SidebarGroup>
+          <SidebarGroupLabel className="text-white">
+            Application
+          </SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title} className="text-white">
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
         <SidebarGroup />
       </SidebarContent>
-      <SidebarFooter />
+      <SidebarFooter className="bg-slate-800" />
     </Sidebar>
   );
 }
