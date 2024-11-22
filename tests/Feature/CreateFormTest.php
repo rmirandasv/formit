170f0c949c3e1eec003b74bae45f6f4586bf2da0
Ronald Miranda <ronaldmirandasv@gmail.com>
2024-11-21 20:02:06 -0600

    fix create new form test

--- tests/Feature/CreateFormTest.php
+++ tests/Feature/CreateFormTest.php
@@ -5,15 +5,21 @@ use App\Actions\Form\CreateForm;
 use App\Models\Team;
 use App\Models\User;
 
+use function Pest\Laravel\actingAs;
+
 uses(RefreshDatabase::class);
 
 test('Create new form', function () {
-    $team = Team::factory()->for(User::factory())->create();
+    $user = User::factory()->create();
+    $team = Team::factory()->for($user)->create();
+    $user->current_team_id = $team->id;
+    $user->save();
+
+    actingAs($team->user);
 
     $createFormAction = new CreateForm();
 
     $form = $createFormAction->handle([
-        'team_id' => $team->id,
         'name' => 'Test Form',
         'slug' => 'test-form',
         'description' => 'This is a test form',
