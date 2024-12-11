75959531eb13b21c25cc8145e3b1304d0c1b1e43
Ronald Miranda <ronaldmirandasv@gmail.com>
2024-12-10 21:47:29 -0600

    empty state

--- resources/ts/pages/form/index.tsx
+++ resources/ts/pages/form/index.tsx
@@ -18,10 +18,9 @@ import { getBreadcrumb } from "@/lib/breadcrumb";
 import { Form, Paginated } from "@/types/global";
 import { Link } from "@inertiajs/react";
 import { EyeOpenIcon, Pencil1Icon } from "@radix-ui/react-icons";
-import { MoreVerticalIcon, PlusIcon } from "lucide-react";
+import { FilePlus, MoreVerticalIcon, PlusIcon } from "lucide-react";
 
 export default function FormIndex({ forms }: { forms: Paginated<Form> }) {
-  console.log(forms);
   return (
     <AppLayout breadcrumbs={getBreadcrumb([{ label: "Forms", url: "/forms" }])}>
       <h1 className="text-white text-2xl lg:text-3xl">Forms</h1>
@@ -38,6 +37,25 @@ export default function FormIndex({ forms }: { forms: Paginated<Form> }) {
           </div>
         </Link>
       </div>
+      {forms.data.length === 0 && (
+        <Card className="mt-4 bg-transparent">
+          <CardContent className="p-4 flex flex-col">
+            <FilePlus className="size-16 self-center text-white" />
+            <p className="text-center text-white text-2xl font-bold">
+              No forms found
+            </p>
+            <p className="text-center text-white">
+              Create a new form to get started
+            </p>
+            <Link
+              href="/forms/create"
+              className="mt-4 p-2 bg-indigo-700 rounded-md text-white self-center"
+            >
+              Create Form
+            </Link>
+          </CardContent>
+        </Card>
+      )}
       <div className="mt-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {forms.data.map((form) => (
           <Card key={form.id}>
